42ec78ae1c48b735fd6ded923272d957cf6d7ea2
LordAmaranth <dragonwalkeramaranth@gmail.com>
2025-01-25 11:45:27 +0900

    ジャンプ操作

--- GGJ2025/Assets/Project/Dragon/Code/Player.cs
+++ GGJ2025/Assets/Project/Dragon/Code/Player.cs
@@ -3,20 +3,60 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour {
     [SerializeField] private PlayerConfig config;
+    [SerializeField] private Rigidbody2D myRigidBody;
 
-
-    private float movementHorizontal;
-    private float movementVertical;
-    private bool inAir;
-    private bool startJump;
+    private float movementHorizontalSpeed;
+    private bool isGrounded;
+    private bool jumping;
+    private bool falling;
+    private bool isMovingVertically => myRigidBody.linearVelocityY != 0f;
     void Start() {
 
     }
 
     void Update() {
-        transform.Translate(new Vector3(movementHorizontal * config.MovementSpeed, 0, 0));
+        if (jumping) {
+            Debug.Log(myRigidBody.linearVelocityY);
+
+            myRigidBody.AddForceY(config.JumpHeldSpeed);
+            if (myRigidBody.linearVelocityY >= config.MaxAscendSpeed) {
+                jumping = false;
+                falling = true;
+            }
+            else if (myRigidBody.linearVelocityY < 0) {
+                falling = true;
+                jumping = false;
+            }
+        }
+
+        myRigidBody.linearVelocity = new Vector2(movementHorizontalSpeed * config.MovementSpeed, myRigidBody.linearVelocity.y);
     }
 
-    public void OnMove(InputAction.CallbackContext context) => movementHorizontal = context.ReadValue<float>();
-    public void OnJump(InputAction.CallbackContext context) => startJump = context.ReadValue<bool>();
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (!collision.gameObject.CompareTag("Ground")) {
+            return;
+        }
+
+        if (collision.GetContact(0).normal == Vector2.up) {
+            isGrounded = true;
+            falling = false;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context) => movementHorizontalSpeed = context.ReadValue<float>();
+    public void OnJump(InputAction.CallbackContext context) {
+        if (context.canceled) {
+            jumping = false;
+        }
+
+        if (!isGrounded || isMovingVertically || falling) {
+            return;
+        }
+
+        if (context.performed) {
+            isGrounded = false;
+            jumping = true;
+            myRigidBody.AddForceY(config.JumpStartSpeed, ForceMode2D.Impulse);
+        }
+    }
 }
--- GGJ2025/Assets/Project/Dragon/Code/PlayerConfig.cs
+++ GGJ2025/Assets/Project/Dragon/Code/PlayerConfig.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerConfig", menuName = "Scriptable Objects/PlayerConfig")]
 public class PlayerConfig : ScriptableObject {
     [field: SerializeField] public float MovementSpeed { get; private set; } = 1;
+    [field: SerializeField] public float JumpStartSpeed { get; private set; } = 20;
+    [field: SerializeField] public float MaxAscendSpeed { get; private set; } = 10;
     [field: SerializeField] public float MaxFallSpeed { get; private set; } = 5;
-    [field: SerializeField] public float JumpSpeed { get; private set; } = 7;
-    [field: SerializeField] public float Gravity { get; private set; } = 9.8f;
+    [field: SerializeField] public float JumpHeldSpeed { get; private set; } = 5;
 }
