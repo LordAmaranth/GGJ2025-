2dfbc9fdafb42ede01b89ffc5776cdad822be332
hattori8 <39850363+hattori8@users.noreply.github.com>
2025-01-26 12:54:12 +0900

    -update シャボン玉のアップデート
    ・

--- GGJ2025/Assets/Project/Hattori/Scripts/BubbleSettings.cs
+++ GGJ2025/Assets/Project/Hattori/Scripts/BubbleSettings.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BubbleSettings", menuName = "ScriptableObjects/BubbleSettings", order = 1)]
 public class BubbleSettings : ScriptableObject {
     public float maxScale = 2f; // 最大スケール
+    public float startMaxScale = 2f; // 最初の最大スケール
+    public float startMinScale = 1f; // 最初の最小スケール
     public float scaleIncreaseSpeed = 1f; // スケールの増加速度
     public float upSpeed = 0.1f; // 上昇速度
     public float WindStrength = 1;
